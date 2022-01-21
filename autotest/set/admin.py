a8cdb1399e98b7b422a43dd93dd4a7c6e1bc9685
liuyinggang <952904268@qq.com>
2022-01-21 21:46:01 +0800

    0.0.1 版本

--- autotest/set/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from set.models import Set
-# Register your models here.
-
-class setAdmin(admin.ModelAdmin):
-    list_display = ['setname', 'setvalue', 'id']
-
-admin.site.register(Set)
