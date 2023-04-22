fa3acd2ad41a3dce8b74beaf82c0b32ebb7e03ed
liuyinggang <952904268@qq.com>
2023-04-22 18:18:50 +0800

    files update

---
+++ bases/daren_square_page.py
@@ -0,0 +1,8 @@
+"""
+# FileName    : daren_square_page.py
+# CreateTime  : 2023 年 04 月 22 日
+# Version     : 
+# Description : 达人广场相关元素
+"""
+# 达人广场[1] - 用户昵称
+daren_square_name_class = 'list-table-info-right-name__nickname'
