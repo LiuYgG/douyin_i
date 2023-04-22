26325278a4aeee4b4df6ae56d7ef4ee3effa43e5
liuyinggang <952904268@qq.com>
2023-04-22 16:41:11 +0800

    更新

--- daren_profile_data.py
+++ daren_profile_data.py
@@ -7,12 +7,12 @@
 import datetime
 
 from selenium import webdriver
-from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import openpyxl
 import time
+from bases import daren_square_page
 from bases import profiles_page
 from bases import send_invitation_page
 from bases import add_operate_window
@@ -44,13 +44,12 @@ class DarenProfiles():
         # 设置列名
         self.worksheet.cell(row=1, column=1, value='姓名')
         self.worksheet.cell(row=1, column=2, value='粉丝数')
-        self.worksheet.cell(row=1, column=3, value='联系方式')
+        # self.worksheet.cell(row=1, column=3, value='联系方式')
         self.worksheet.cell(row=1, column=4, value='网址')
 
     def get_daren_info(self):
 
-        nicke_name_elements = self.d.find_elements(By.CLASS_NAME, 'list-table-info-right-name__nickname')
-        # time.sleep(3)
+        nicke_name_elements = self.d.find_elements(By.CLASS_NAME, daren_square_page.daren_square_name_class)
         count = int(input("请输入你要的人数: "))
         start_index = int(input("请输入要开始的位置："))
         # 循环获取
@@ -66,7 +65,6 @@ class DarenProfiles():
                 self.d.switch_to.window(handle_tab_all[1])
                 time.sleep(3)
                 # 强制刷新
-                # ActionChains(self.d).key_down(Keys.CONTROL).send_keys(Keys.F5).key_up(Keys.CONTROL).perform()
                 self.d.execute_script("location.reload(true);")
                 print("当前窗口已强制刷新")
                 time.sleep(10)
@@ -76,7 +74,9 @@ class DarenProfiles():
                 '''
                 try:
                     message_box = self.d.find_element(By.XPATH, profiles_page.profiles_message_box_xpath)
-                    if message_box is not None:
+                    if message_box:
+                        print("*" * 50)
+                        print()
                         hide_btn = self.d.find_elements(By.CLASS_NAME, profiles_page.profiles_message_hide_class)
                         time.sleep(5)
                         try:
@@ -88,14 +88,12 @@ class DarenProfiles():
                             print('-' * 30)
                         except Exception as h :
                             print(f"联系方式错误信息:{h}")
-                    else:
-                        print("此用户没有联系方式: ", self.d.find_element(By.XPATH, profiles_page.daren_name).text)
-                        time.sleep(2)
-                        # 关闭窗口
-                        self.d.close()
-                        self.d.switch_to.window((handle_tab_all[0]))
                 except Exception as m:
-                    print(f"联系方式异常错误信息: {m}")
+                    if 'no such element: Unable to locate element: {"method":"xpath","selector":"//*[@id="app"]/div/div/div[1]/div[1]/div[2]/div[7]/div"}' in str(m):
+                        time.sleep(1)
+                        print(f"当前用户【 {self.d.find_element(By.XPATH, profiles_page.profiles_daren_name_xpath).text} 】没有设置联系方式信息")
+                    else:
+                        print(f"联系方式异常错误信息: {m}")
                 time.sleep(10)
 
                 '''
@@ -105,11 +103,10 @@ class DarenProfiles():
                     profiles_send = self.d.find_element(By.CLASS_NAME, profiles_page.profiles_send_class)
                     profiles_send_disabled = self.d.find_element(By.CLASS_NAME, profiles_page.profiles_send_disabled_class)
                     time.sleep(1)
-                    # print("当前用户支持邀约")
                     if profiles_send_disabled :
                         time.sleep(1)
                         print("-" * 100)
-                        print("当前用户已禁止邀约")
+                        print(f"当前用户【 {self.d.find_element(By.XPATH, profiles_page.profiles_daren_name_xpath).text} 】已禁止邀约")
                         # 关闭窗口
                         self.d.close()
                         self.d.switch_to.window((handle_tab_all[0]))
@@ -118,7 +115,7 @@ class DarenProfiles():
                 except Exception as s:
                     if 'no such element: Unable to locate element: {"method":"css selector","selector":".auxo-tooltip-disabled-compatible-wrapper"}' in str(s):
                         time.sleep(3)
-                        print("当前用户可以邀约")
+                        print(f"当前用户【 {self.d.find_element(By.XPATH, profiles_page.profiles_daren_name_xpath).text} 】可以邀约")
                         time.sleep(3)
                         profiles_send.click()
                         print("已点击发送邀约按钮")
@@ -127,8 +124,8 @@ class DarenProfiles():
                         time.sleep(3)
                         print("已点击添加上次商品")
                         time.sleep(5)
-                        self.d.find_element(By.XPATH, send_invitation_page.send_invitation_btn_xpath).click()
-                        # self.d.find_element(By.XPATH, send_invitation_page.send_cancel_xpath).click()
+                        # self.d.find_element(By.XPATH, send_invitation_page.send_invitation_btn_xpath).click()
+                        self.d.find_element(By.XPATH, send_invitation_page.send_cancel_xpath).click()
                         print("已点击发送邀约")
                     else:
                         print(f'发送邀约异常错误信息：{s}')
@@ -167,15 +164,13 @@ class DarenProfiles():
         for l, data in enumerate(self.data_list):
             self.worksheet.cell(row=l + 2, column=1, value=data['姓名'])
             self.worksheet.cell(row=l + 2, column=2, value=data['粉丝数'])
-            self.worksheet.cell(row=l + 2, column=3, value=data['联系方式'])
+            # self.worksheet.cell(row=l + 2, column=3, value=data['联系方式'])
             self.worksheet.cell(row=l + 2, column=4, value=data['网址'])
+
     def save_workboot(self):
-
-        # workbook_path = input()
-
+        # 保存数据
         self.workbook.save(f'datas/{time.strftime("%Y-%m-%d")}.xlsx')
 
-        # return workbook_path
 if __name__ == '__main__':
     dd = DarenProfiles()
     dd.run()
